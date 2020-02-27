d69d1395c1f89cb11507b7f9be21f07ebc2ed585
langhuihui <178529795@qq.com>
2020-02-27 20:13:14 +0800

    first commit

---
+++ .vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    dest: 'dist',
+    serviceWorker: true,
+    themeConfig: {
+        sidebar: [
+            ['/', '起步'],
+            ['/develop', '插件开发'],
+            ['/history', '更新日志'],
+            ['/plugins', '内置插件'],
+            ['/design', '设计原理']
+        ]
+    },
+    title: 'Monibuca',
+    base: '/docs/'
+}
